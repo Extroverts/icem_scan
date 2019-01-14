bf14ea0f44f9f9049179b6cd5bc713853cbbff4f
Extroverts <onkarmahajan@gmail.com>
2019-01-15 02:13:12 +0530

    final commit

--- app/src/main/java/com/om/developers/scan/ICEM.java
+++ app/src/main/java/com/om/developers/scan/ICEM.java
@@ -1,13 +1,91 @@
 package com.om.developers.scan;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ICEM extends AppCompatActivity {
+
+    TextView tvs,textView;
+    String mydata;
+    ListView listView;
+    private List<String> arrayList=new ArrayList <>();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+            .setTimestampsInSnapshotsEnabled(true)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icem);
+        db.setFirestoreSettings(settings);
+
+        tvs = findViewById(R.id.data);
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.lists);
+
+
+        final String clg_id = getIntent().getStringExtra("datas");
+        tvs.setText(clg_id);
+
+        db.collection("abc")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener <QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task <QuerySnapshot> task) {
+
+                        if ( task.isSuccessful() )
+                            {
+                                for (QueryDocumentSnapshot documentSnapshot : task.getResult())
+                                    {
+                                        mydata = (String) documentSnapshot.getData().get("name");
+                                        String valid=(String)documentSnapshot.getData().get("clg_id");
+                                        arrayList.add(mydata);
+                                        Log.d("", documentSnapshot.getId() + "==>" + documentSnapshot.getData().get("clg_id"));
+                                         if ( arrayList.contains(clg_id) )
+                                            {
+                                                if(valid.equals("Present")){
+                                                    Toast.makeText(getApplicationContext(),"Already Present",Toast.LENGTH_SHORT).show();
+                                                }
+                                                else {
+                                                    Toast.makeText(getApplicationContext(), "Valid College ID", Toast.LENGTH_SHORT).show();
+                                                    db.collection("abc").document(documentSnapshot.getId()).update("clg_id","Present");
+                                                }
+                                            }
+                                    }
+
+                                ArrayAdapter <String> arrayAdapter = new ArrayAdapter <String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+                                arrayAdapter.notifyDataSetChanged();
+                                listView.setAdapter(arrayAdapter);
+                            } else
+                            {
+                                Log.d("", "ERROR", task.getException());
+                            }
+                    }
+                });
     }
-}
+
+
+    }
+
--- app/src/main/java/com/om/developers/scan/MainActivity.java
+++ app/src/main/java/com/om/developers/scan/MainActivity.java
@@ -1,59 +1,36 @@
 package com.om.developers.scan;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.L;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "";
     SurfaceView surfaceView;
     TextView txtBarcodeValue;
-    private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
     String intentData = "";
-    String data="CO16OPS487";
+
     boolean isEmail = false;
     String compares,changes;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
 
-        barcodeDetector = new BarcodeDetector.Builder(this)
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.ALL_FORMATS)
                 .build();
 
@@ -128,16 +105,10 @@ public class MainActivity extends AppCompatActivity {
                                 isEmail = false;
                                 intentData = barcodes.valueAt(0).displayValue;
                                 txtBarcodeValue.setText(intentData);
-                                if(intentData!=null) {
-                                        Toast.makeText(getApplicationContext(),"Valid College ID",Toast.LENGTH_LONG).show();
-                                        Intent i=new Intent(MainActivity.this,ICEM.class);
-                                        startActivity(i);
-                                        finish();
-                                        }
-                                else
-                                {
-                                    Toast.makeText(getApplicationContext(),"College ID is not Valid",Toast.LENGTH_LONG).show();
-                                }
+                                Intent news=new Intent(MainActivity.this,ICEM.class);
+                                news.putExtra("datas",intentData);
+                                startActivity(news);
+                                finish();
                             }
                         }
                     });
@@ -146,39 +117,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    public void searches(){
-        final DocumentReference docRef = db.collection("Notice").document("co");
-        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot snapshot,
-                                @Nullable FirebaseFirestoreException e) {
-                if (e != null) {
-                    Log.w(TAG, "Listen failed.", e);
-                    return;
-                }
-
-                String source = snapshot != null && snapshot.getMetadata().hasPendingWrites()
-                        ? "Local" : "Server";
-
-                if (snapshot != null && snapshot.exists()) {
-                    Log.d(TAG, source + " data: " + snapshot.getData().get("name"));
-                    compares= (String) snapshot.getData().get("name");
-                    changes=(String)snapshot.getData().get("status");
-
-                   docRef.update("status","Present");
-
-
-
-                } else {
-                    Log.d(TAG, source + " data: null");
-                }
-            }
-        });
-    }
-
-
-
     @Override
     protected void onPause() {
         super.onPause();
