c778862d8f09b280da3828364abc71aca2b2842a
Extroverts <onkarmahajan@gmail.com>
2019-01-19 01:02:09 +0530

    few changes in code that only one record at a time back press button

--- app/src/main/java/com/om/developers/scan/ICEM.java
+++ app/src/main/java/com/om/developers/scan/ICEM.java
@@ -1,9 +1,13 @@
 package com.om.developers.scan;
 
+
+import android.content.Intent;
 import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,26 +15,26 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Timestamp;
+
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class ICEM extends AppCompatActivity {
 
+    private static final String TAG = "";
     TextView tvs,textView;
     String mydata;
     ListView listView;
     private List<String> arrayList=new ArrayList <>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    String dec = "";
     FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
             .setTimestampsInSnapshotsEnabled(true)
             .build();
@@ -47,45 +51,111 @@ public class ICEM extends AppCompatActivity {
 
 
         final String clg_id = getIntent().getStringExtra("datas");
-        tvs.setText(clg_id);
+        int num=0;
 
-        db.collection("abc")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener <QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task <QuerySnapshot> task) {
+        for(int i=0;i<clg_id.length();i++){
+            num = (int)clg_id.charAt(i) - 97 - i;
 
-                        if ( task.isSuccessful() )
-                            {
-                                for (QueryDocumentSnapshot documentSnapshot : task.getResult())
-                                    {
-                                        mydata = (String) documentSnapshot.getData().get("name");
-                                        String valid=(String)documentSnapshot.getData().get("clg_id");
-                                        arrayList.add(mydata);
-                                        Log.d("", documentSnapshot.getId() + "==>" + documentSnapshot.getData().get("clg_id"));
-                                         if ( arrayList.contains(clg_id) )
-                                            {
-                                                if(valid.equals("Present")){
-                                                    Toast.makeText(getApplicationContext(),"Already Present",Toast.LENGTH_SHORT).show();
-                                                }
-                                                else {
-                                                    Toast.makeText(getApplicationContext(), "Valid College ID", Toast.LENGTH_SHORT).show();
-                                                    db.collection("abc").document(documentSnapshot.getId()).update("clg_id","Present");
-                                                }
-                                            }
-                                    }
+            dec += Integer.toString(num);
+        }
 
-                                ArrayAdapter <String> arrayAdapter = new ArrayAdapter <String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
-                                arrayAdapter.notifyDataSetChanged();
-                                listView.setAdapter(arrayAdapter);
-                            } else
-                            {
-                                Log.d("", "ERROR", task.getException());
+
+        tvs.setText(dec);
+
+        DocumentReference docRef = db.collection("students").document(dec);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        mydata = (String) document.getData().get("number");
+                        String valid=(String)document.getData().get("status");
+                        Log.d("", mydata);
+                        arrayList.add(mydata);
+                        Log.d("", document.getId() + "==>" + document.getData().get("name"));
+
+                        if(dec.contains(mydata)){
+                            if(valid.equals("Present")){
+                                Toast.makeText(getApplicationContext(),"Already Present",Toast.LENGTH_SHORT).show();
                             }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Valid College ID", Toast.LENGTH_SHORT).show();
+                                db.collection("students").document(document.getId()).update("status","Present");
+                            }
+
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"QR Not Valid",Toast.LENGTH_SHORT).show();
+                        }
+//
+//                        if ( arrayList.contains(dec) )
+//                            {
+//                                if(valid.equals("Present")){
+//                                    Toast.makeText(getApplicationContext(),"Already Present",Toast.LENGTH_SHORT).show();
+//                                }
+//                                else {
+//                                    Toast.makeText(getApplicationContext(), "Valid College ID", Toast.LENGTH_SHORT).show();
+//                                    db.collection("students").document(document.getId()).update("status","Present");
+//                                }
+//                            }
+
+                    ArrayAdapter <String> arrayAdapter = new ArrayAdapter <String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+                    arrayAdapter.notifyDataSetChanged();
+                    listView.setAdapter(arrayAdapter);
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData().get("name"));
+                    } else {
+                        Log.d(TAG, "No such document");
                     }
-                });
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
+//
+//        db.collection("students")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener <QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task <QuerySnapshot> task) {
+//
+//                        if ( task.isSuccessful() )
+//                            {
+//                                for (QueryDocumentSnapshot documentSnapshot : task.getResult())
+//                                    {
+//                                        mydata = (String) documentSnapshot.getData().get("number");
+//                                        String valid=(String)documentSnapshot.getData().get("status");
+//                                        arrayList.add(mydata);
+//                                        Log.d("", documentSnapshot.getId() + "==>" + documentSnapshot.getData().get("name"));
+//                                         if ( arrayList.contains(dec) )
+//                                            {
+//                                                if(valid.equals("Present")){
+//                                                    Toast.makeText(getApplicationContext(),"Already Present",Toast.LENGTH_SHORT).show();
+//                                                }
+//                                                else {
+//                                                    Toast.makeText(getApplicationContext(), "Valid College ID", Toast.LENGTH_SHORT).show();
+//                                                    db.collection("students").document(documentSnapshot.getId()).update("status","Present");
+//                                                }
+//                                            }
+//                                    }
+//
+//                                ArrayAdapter <String> arrayAdapter = new ArrayAdapter <String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+//                                arrayAdapter.notifyDataSetChanged();
+//                                listView.setAdapter(arrayAdapter);
+//                            } else
+//                            {
+//                                Log.d("", "ERROR", task.getException());
+//                            }
+//                    }
+//                });
     }
 
+    public void onBackPressed(){
+        startActivity(new Intent(ICEM.this,MainActivity.class));
+    }
 
     }
 
--- app/src/main/java/com/om/developers/scan/MainActivity.java
+++ app/src/main/java/com/om/developers/scan/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.TextView;
+
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
@@ -22,6 +24,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "";
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 7;
     SurfaceView surfaceView;
     TextView txtBarcodeValue;
     private CameraSource cameraSource;
@@ -29,102 +32,140 @@ public class MainActivity extends AppCompatActivity {
     String intentData = "";
 
     boolean isEmail = false;
-    String compares,changes;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView( R.layout.activity_main );
         initViews();
 
     }
-    private void initViews() {
-        txtBarcodeValue = findViewById(R.id.txtBarcodeValue);
-        surfaceView = findViewById(R.id.surfaceView);
+
+    private void initViews ( ) {
+        txtBarcodeValue = findViewById( R.id.txtBarcodeValue );
+        surfaceView = findViewById( R.id.surfaceView );
 
     }
-    private void initialiseDetectorsAndSources() {
+
+    private void initialiseDetectorsAndSources ( ) {
 
         //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
 
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.ALL_FORMATS)
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder( this )
+                .setBarcodeFormats( Barcode.ALL_FORMATS )
                 .build();
 
-        cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1920, 1080)
-                .setAutoFocusEnabled(true) //you should add this feature
+        cameraSource = new CameraSource.Builder( this, barcodeDetector )
+                .setRequestedPreviewSize( 1920, 1080 )
+                .setAutoFocusEnabled( true ) //you should add this feature
                 .build();
 
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+        surfaceView.getHolder().addCallback( new SurfaceHolder.Callback() {
             @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                        cameraSource.start(surfaceView.getHolder());
-                    } else {
-                        ActivityCompat.requestPermissions(MainActivity.this, new
-                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+            public void surfaceCreated (SurfaceHolder holder) {
+                try
+                    {
+                        if ( ContextCompat.checkSelfPermission( getApplicationContext(),
+                                Manifest.permission.CAMERA )
+                             != PackageManager.PERMISSION_GRANTED )
+                            {
+
+                                // Permission is not granted
+                                // Should we show an explanation?
+                                if ( ActivityCompat.shouldShowRequestPermissionRationale( MainActivity.this,
+                                        Manifest.permission.CAMERA ) )
+                                    {
+                                        // Show an explanation to the user *asynchronously* -- don't block
+                                        // this thread waiting for the user's response! After the user
+                                        // sees the explanation, try again to request the permission.
+                                    } else
+                                    {
+                                        // No explanation needed; request the permission
+                                        ActivityCompat.requestPermissions( MainActivity.this,
+                                                new String[]{ Manifest.permission.CAMERA },
+                                                MY_PERMISSIONS_REQUEST_READ_CONTACTS );
+
+                                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                                        // app-defined int constant. The callback method gets the
+                                        // result of the request.
+                                    }
+                            } else
+                            {
+                                // Permission has already been granted
+                            }
+                        if ( ActivityCompat.checkSelfPermission( MainActivity.this, Manifest.permission.CAMERA ) == PackageManager.PERMISSION_GRANTED )
+                            {
+                                cameraSource.start( surfaceView.getHolder() );
+                            } else
+                            {
+                                ActivityCompat.requestPermissions( MainActivity.this, new
+                                        String[]{ Manifest.permission.CAMERA }, REQUEST_CAMERA_PERMISSION );
+                            }
+
+                    } catch ( IOException e )
+                    {
+                        e.printStackTrace();
                     }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-            }
-        }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             }
 
             @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
+            public void surfaceChanged (SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed (SurfaceHolder holder) {
                 cameraSource.stop();
             }
-        });
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+        } );
+        barcodeDetector.setProcessor( new Detector.Processor <Barcode>() {
             @Override
-            public void release() {
+            public void release ( ) {
                 //Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
             }
+
             @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+            public void receiveDetections (Detector.Detections <Barcode> detections) {
+                final SparseArray <Barcode> barcodes = detections.getDetectedItems();
 
-                if (barcodes.size() != 0) {
-                    txtBarcodeValue.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (barcodes.valueAt(0).email != null) {
-                                txtBarcodeValue.removeCallbacks(null);
-                                intentData = barcodes.valueAt(0).email.address;
-                                txtBarcodeValue.setText(intentData);
-                                isEmail = true;
-                            } else {
-                                isEmail = false;
-                                intentData = barcodes.valueAt(0).displayValue;
-                                txtBarcodeValue.setText(intentData);
-                                Intent news=new Intent(MainActivity.this,ICEM.class);
-                                news.putExtra("datas",intentData);
-                                startActivity(news);
-                                finish();
+                if ( barcodes.size() != 0 )
+                    {
+                        txtBarcodeValue.post( new Runnable() {
+                            @Override
+                            public void run ( ) {
+                                if ( barcodes.valueAt( 0 ).email != null )
+                                    {
+                                        txtBarcodeValue.removeCallbacks( null );
+                                        intentData = barcodes.valueAt( 0 ).email.address;
+                                        txtBarcodeValue.setText( intentData );
+                                        isEmail = true;
+                                    } else
+                                    {
+                                        isEmail = false;
+                                        intentData = barcodes.valueAt( 0 ).displayValue;
+                                        txtBarcodeValue.setText( intentData );
+                                        Intent news = new Intent( MainActivity.this, ICEM.class );
+                                        news.putExtra( "datas", intentData );
+                                        startActivity( news );
+                                        finish();
+                                    }
                             }
-                        }
-                    });
+                        } );
 
-                }
+                    }
             }
-        });
+        } );
     }
+
     @Override
-    protected void onPause() {
+    protected void onPause ( ) {
         super.onPause();
         cameraSource.release();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume ( ) {
         super.onResume();
         initialiseDetectorsAndSources();
     }
